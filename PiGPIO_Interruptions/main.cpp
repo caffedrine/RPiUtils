e42eb3bb2c542a36b2d6b6094bb8046aa82b8062
caffedrine <bacchusstyle@yahoo.com>
2018-09-28 18:31:06 +0300

    precise stepper driver added

--- PiGPIO_Interruptions/main.cpp
+++ PiGPIO_Interruptions/main.cpp
@@ -5,14 +5,19 @@
 #include "hal.h"
 #include "PushButton.h"
 #include "Gpio.h"
+#include "Stepper.h"
 
 using namespace std;
 
 #define DEBOUNCE_US	1000	/* 1 ms */
-#define PWM_PIN		21
 #define PWM_FREQ	4000
-#define BUTTON_PIN	20
+#define BUTTON_PIN	19
 #define INT_PIN		16
+
+#define STEPPER_PULSE		21
+#define STEPPER_FEEDBACK	20
+#define STEPPER_DIRECTION	0
+#define STEPPER_ENABLE		0
 
 void PushButtonOnStateChanged(PushButtonState NewState)
 {
@@ -28,14 +33,15 @@ void GpioLevelChanged(LogicalLevel NewLogicalLevel)
 	LastTicks = Vfb_GetMicros();
 }
 
+void OnStepsDone(long Steps)
+{
+	cout << "Finished to do " << Steps << " steps" << endl;
+}
+
 int main()
 {
+	cout << "---STARTED---" << endl;
 	Vfb_GpioInitialise();
-	
-	/* PWM stuff */
-	Vfb_SetPinMode(PWM_PIN, PinMode::OUTPUT);
-	cout << "Freq PWM: " << Vfb_InitPwm(PWM_PIN, PWM_FREQ) << endl;
-	Vfb_PwmOut(PWM_PIN, 128);
 	
 	/* Push button */
 	PushButton button(BUTTON_PIN, 500000);
@@ -47,11 +53,21 @@ int main()
 	Gpio pin(INT_PIN, 1000);
 	pin.SetStateChangedCallback( GpioLevelChanged );
 	
+	/* Stepper */
+	Stepper stepper(STEPPER_PULSE, STEPPER_FEEDBACK, STEPPER_DIRECTION);
+	stepper.PwmConfig(100, 10);
+	stepper.SetStepsDoneCallback( OnStepsDone );
+	stepper.RunSteps(500);
+	
 	bool toogleFlag = false;
 	while(true)
 	{
-		if( getchar() == 'q')
-			break;
+		int c = getchar();
+		
+		if(c == '1') stepper.RunSteps(100);
+		else if(c == '2') stepper.Stop();
+		else if(c == 'q') break;
+		
 		//this_thread::sleep_for( chrono::milliseconds(1) );
 	}
 }
