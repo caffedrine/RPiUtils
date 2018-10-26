2cf50038df9c9a524c8345519a9ea7de3b464bc1
caffedrine <bacchusstyle@yahoo.com>
2018-10-26 10:19:03 +0300

    rpi raspivid broadcast scripts impoved

--- camera_read_stream.sh
+++ camera_read_stream.sh
@@ -1,3 +1,8 @@
 # This shall be started first
-# Usage: camera_read_stream.sh streamer_port
-nc -l -u -p $1 | mplayer - -fps 20 -cache 1024
+USAGE="Usage: camera_read_stream.sh listening_port"
+printf "$USAGE\n"
+# UDP
+# nc -l -p $1 | mplayer - -fps 20 -cache 2048
+
+# TCP
+nc -l -p $1 | mplayer -fps 200 -demuxer h264es -
--- camera_stream.sh
+++ camera_stream.sh
@@ -1,1 +1,9 @@
-raspivid -n -w 854 -h 480 -fps 20 -t 9999999 -o - | nc -u 192.168.1.101 5001
+USAGE="Usage: ./camera_stream.sh CLIENT_IP CLIENT_PORT"
+printf "$USAGE\n"
+
+# Using UDP
+#raspivid -n -w 854 -h 480 -fps 20 -t 9999999 -o - | nc -u $1 $2
+
+
+# Using TCP
+raspivid -n -w 480 -h 320 -fps 20 -hf -t 9999999 -o - | nc $1 $2
