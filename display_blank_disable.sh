fb54ea04d7018f41b19affcf195108f010e760e2
caffedrine <bacchusstyle@yahoo.com>
2019-05-18 23:06:53 +0300

    updates

--- display_blank_disable.sh
+++ display_blank_disable.sh
@@ -5,5 +5,13 @@ if ! [ -x "$(command -v xscreensaver)" ]; then
 	sudo apt-get install xscreensaver
 fi
 
-# Disable screensaver - it means no blank
-xscreensaver-command -deactivate
+# Disable blank screen from screensaver
+DISPLAY=:0 xscreensaver-command -deactivate
+
+# Disable blank screen from XSET
+DISPLAY=:0 xset s off 			# don't activate screensaver
+DISPLAY=:0 xset -dpms 			# disable DPMS (Energy Star) features.
+DISPLAY=:0 xset s noblank 		# don't blank the video device
+
+# Auto hide mouse after 10 seconds of inactivity
+DISPLAY=:0 unclutter -idle 10 -root &
